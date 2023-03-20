d9adec14f519b2948fc6c1d09b93b0f0be3c2ce7
mahmoudd2 <mahmouddahmedd220@gmail.com>
2023-03-20 18:37:29 +0200

    Update Character.java

--- src/model/collectibles/Character.java
+++ src/model/collectibles/Character.java
@@ -10,4 +10,40 @@ Character(String name, int maxHp, int attackDmg)
 {
 	
 }
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public java.awt.Point getLocation() {
+	return location;
+}
+public void setLocation(java.awt.Point location) {
+	this.location = location;
+}
+public int getMaxHp() {
+	return maxHp;
+}
+public void setMaxHp(int maxHp) {
+	this.maxHp = maxHp;
+}
+public int getCurrentHp() {
+	return currentHp;
+}
+public void setCurrentHp(int currentHp) {
+	this.currentHp = currentHp;
+}
+public int getAttackDmg() {
+	return attackDmg;
+}
+public void setAttackDmg(int attackDmg) {
+	this.attackDmg = attackDmg;
+}
+public Character getTarget() {
+	return target;
+}
+public void setTarget(Character target) {
+	this.target = target;
+}
 }
