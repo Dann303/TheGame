c12506140f4c3273047def108d412403af802fed
Dann303 <daniel.ekdawi@gmail.com>
2023-03-20 21:36:34 +0200

    Did the CSV file read thingy

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -4,6 +4,9 @@ import model.characters.Hero;
 import model.characters.Zombie;
 
 import java.util.ArrayList;
+import java.io. * ;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Game {
     public static ArrayList<Hero> availableHeroes;
@@ -12,6 +15,44 @@ public class Game {
     public static Cell [][] map;
 
     public static void loadHeroes(String filePath) throws Exception {
-        
+        availableHeroes = getHeroesFromCSVFile(filePath);
     }
+
+    private static ArrayList<Hero> getHeroesFromCSVFile(String filePath) throws Exception {
+        Scanner sc = new Scanner(new File(filePath));
+        sc.useDelimiter(",");
+
+        ArrayList<Hero> result = new ArrayList<Hero>();
+
+        while (sc.hasNext()) {
+            for (int i = 0; sc.hasNext() && i < 5; i++) {
+                String name = sc.next();
+                String type = sc.next();
+                int maxHp = Integer.parseInt(sc.next());
+                int maxActions = Integer.parseInt(sc.next());
+                int attackDmg = Integer.parseInt(sc.next());
+
+                Hero h = null;
+                switch (type) {
+                    case "FIGH":
+                        h = new Fighter(name, maxHp, attackDmg, maxActions);
+                        break;
+                    case "EXP":
+                        h = new Explorer(name, maxHp, attackDmg, maxActions);
+                        break;
+                    case "MED":
+                        h = new Medic(name, maxHp, attackDmg, maxActions);
+                        break;
+                }
+
+                result.add(h);
+            }
+        }
+
+        sc.close();
+
+        return result;
+    }
+
 }
+
--- src/model/characters/Zombie.java
+++ src/model/characters/Zombie.java
@@ -3,8 +3,12 @@ package model.characters;
 public class Zombie extends Character{
     private static int ZOMBIES_COUNT;
     public Zombie() {
+<<<<<<< Updated upstream
         String name = "Zombie " + this.ZOMBIES_COUNT;
         super(name, 40, 10);
+=======
+        super("Zombie " + ZOMBIES_COUNT, 40, 10);
+>>>>>>> Stashed changes
     }
 
 }
