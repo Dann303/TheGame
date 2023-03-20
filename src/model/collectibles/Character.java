ec6d1ece5c93dff565273613db425d76fdb7cdaa
mahmoudd2 <mahmouddahmedd220@gmail.com>
2023-03-20 18:31:04 +0200

    Update Character.java

--- src/model/collectibles/Character.java
+++ src/model/collectibles/Character.java
@@ -1,5 +1,13 @@
 package model.collectibles;
-
 public abstract class Character {
-
+private String name;
+private java.awt.Point location;
+private int maxHp;
+private int currentHp;
+private int attackDmg;
+private Character target;
+Character(String name, int maxHp, int attackDmg)
+{
+	
+}
 }
