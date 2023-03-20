1bb06497edc953ec2682d4c131620ccf91c7acd8
mahmoudd2 <mahmouddahmedd220@gmail.com>
2023-03-20 19:57:57 +0200

    Create GameActionException.java

---
+++ src/exceptions/GameActionException.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+public class GameActionException extends Exception {
+
+	public GameActionException() {
+		super();
+	}
+
+	public GameActionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	
+	public GameActionException(String message) {
+		super(message);
+	}
+
+}
