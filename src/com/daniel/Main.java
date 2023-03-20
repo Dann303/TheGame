9bac82af25e392623ced1e87246a01080cbf2eab
mahmoudd2 <mahmouddahmedd220@gmail.com>
2023-03-20 17:47:04 +0200

    Update Main.java

--- src/com/daniel/Main.java
+++ src/com/daniel/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
     	System.out.println("hi ana rubina");
         System.out.println("hi w ana daniel");
+        System.out.println("hi w ana Mahmoud");
+        
     }
 }
