a557829301219b2fb8ad5d7328d17079f3b532ce
Dann303 <daniel.ekdawi@gmail.com>
2023-03-20 20:38:59 +0200

    Update Game.java

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -2,6 +2,7 @@ package engine;
 
 import model.characters.Hero;
 import model.characters.Zombie;
+import model.world.Cell;
 
 import java.util.ArrayList;
 
@@ -9,7 +10,7 @@ public class Game {
     public static ArrayList<Hero> availableHeroes;
     public static ArrayList<Hero> heroes;
     public static ArrayList<Zombie> zombies;
-    public static Cell [][] map;
+    public static Cell[][] map;
 
     public static void loadHeroes(String filePath) throws Exception {
         
