475286651c26905b77ad59129469560b80fbf14f
Dann303 <daniel.ekdawi@gmail.com>
2023-03-20 19:31:48 +0200

    yoh ba2a fee conflict

--- src/model/collectibles/Character.java
+++ src/model/collectibles/Character.java
@@ -4,50 +4,64 @@ import java.awt.Point;
 
 public abstract class Character {
 private String name;
-private java.awt.Point location;
+private Point location;
 private int maxHp;
 private int currentHp;
 private int attackDmg;
 private Character target;
+
 public Character(String name,int maxHp,int attackDmg) {
 	this.name = name;
 	this.maxHp = maxHp;
 	this.attackDmg = attackDmg;
 }
+
 public String getName() {
 	return name;
 }
+
 public void setName(String name) {
 	this.name = name;
 }
-public java.awt.Point getLocation() {
+
+public Point getLocation() {
 	return location;
 }
-public void setLocation(java.awt.Point location) {
+
+public void setLocation(Point location) {
 	this.location = location;
 }
+
 public int getMaxHp() {
 	return maxHp;
 }
+
 public void setMaxHp(int maxHp) {
 	this.maxHp = maxHp;
 }
+
 public int getCurrentHp() {
 	return currentHp;
 }
+
 public void setCurrentHp(int currentHp) {
 	this.currentHp = currentHp;
 }
+
 public int getAttackDmg() {
 	return attackDmg;
 }
+
 public void setAttackDmg(int attackDmg) {
 	this.attackDmg = attackDmg;
 }
+
 public Character getTarget() {
 	return target;
 }
+
 public void setTarget(Character target) {
 	this.target = target;
 }
+
 }
