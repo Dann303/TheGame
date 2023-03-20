18c8ed6a300f407d8c7dc971744d7879995bee66
Dann303 <daniel.ekdawi@gmail.com>
2023-03-20 20:05:08 +0200

    Create Game.java

---
+++ src/engine/Game.java
@@ -0,0 +1,17 @@
+package engine;
+
+import model.characters.Hero;
+import model.characters.Zombie;
+
+import java.util.ArrayList;
+
+public class Game {
+    public static ArrayList<Hero> availableHeroes;
+    public static ArrayList<Hero> heroes;
+    public static ArrayList<Zombie> zombies;
+    public static Cell [][] map;
+
+    public static void loadHeroes(String filePath) throws Exception {
+        
+    }
+}
