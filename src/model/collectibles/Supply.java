cd36fc0d5440395f784b21d7d6681907b54f24f9
Dann303 <daniel.ekdawi@gmail.com>
2023-05-02 18:38:26 +0300

    Collectibles (part 1)

--- src/model/collectibles/Supply.java
+++ src/model/collectibles/Supply.java
@@ -1,9 +1,20 @@
 package model.collectibles;
 
+import model.characters.Hero;
+
 public class Supply implements Collectible{
 	
     public Supply () {
 
     }
 
+    @Override
+    public void pickUp(Hero h) {
+        h.getSupplyInventory().add(this);
+    }
+
+    @Override
+    public void use(Hero h) {
+        h.getSupplyInventory().remove(0);
+    }
 }
--- src/model/collectibles/Vaccine.java
+++ src/model/collectibles/Vaccine.java
@@ -1,9 +1,22 @@
 package model.collectibles;
 
+import model.characters.Hero;
+
 public class Vaccine implements Collectible{
 
     public Vaccine() {
 
+    }
+
+    @Override
+    public void pickUp(Hero h) {
+        h.getVaccineInventory().add(this);
+    }
+
+    @Override
+    public void use(Hero h) {
+        int index = h.getVaccineInventory().indexOf(this);
+        h.getVaccineInventory().remove(index);
     }
 
 }
